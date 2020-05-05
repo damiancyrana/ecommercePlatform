ad30515444e85ce2c70bfa069bd20b27154fa387
Damian Cyrana <damiancyrana@gmail.com>
2020-05-05 17:22:05 +0200

    Implemented PostgreSQL database

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ProductGroup, Product
 
-# Register your models here.
+
+@admin.register(ProductGroup)
+class ProductGroupAdmin(admin.ModelAdmin):
+    list_display = ['label', 'slug']
+    prepopulated_fields = {'slug': ('label',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['label', 'slug', 'cost', 'accessible', 'created', 'updated']
+    list_filter = ['accessible', 'created', 'updated']
+    list_editable = ['cost', 'accessible']
+    prepopulated_fields = {'slug': ('label',)}
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductGroup(models.Model):
+    label = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('label',)
+        verbose_name = 'product_group'
+
+    def __str__(self):
+        return self.label
+
+
+class Product(models.Model):
+    product_group = models.ForeignKey(ProductGroup, related_name='products', on_delete=models.CASCADE)
+    label = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    accessible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('label',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.label
